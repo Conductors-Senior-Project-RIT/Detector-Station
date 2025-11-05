8e1fcc311598fcb79c688ec789a033e7e94326b3
Kaitlin Yandik <kmy1691@g.rit.edu>
2025-11-04 22:56:31 -0500

    customizing fields and adding copy of the tracksense flowgraph

--- startup_script.ps1
+++ startup_script.ps1
@@ -1,17 +1,19 @@
 # Replace the content in quotes with the computer username.
-$username = "dell"
+$username = "username"
+$onedrive = ""
+$shortcutdir = "GNU Radio"
 
 $wshShell = New-Object -ComObject WScript.Shell
 
 # Start radio companion
-$radiopath = "C:\Users\" + $username + "\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\GNU Radio\GNU Radio Companion.lnk"
+$radiopath = "C:\Users\" + $username + "\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\" + $shortcutdir + "\GNU Radio Companion.lnk"
 Start-Process -FilePath $radiopath
 
 # Wait for load up
-Start-Sleep -Seconds 4
+Start-Sleep -Seconds 12
 
 # Set focus to GNU window
-$windowname = "TrackSenseGNURadioWorkflow.grc - C:\Users\" + $username + "\Desktop"
+$windowname = "TrackSenseGNURadioWorkflow.grc - C:\Users\" + $username + "\Desktop\Tracksense"
 [void]$wshShell.AppActivate( $windowname )
 
 # Send F5 key (execute shortcut: generate graph)
@@ -27,5 +29,6 @@ $wshShell.SendKeys( '{F6}' )
 Start-Sleep -Seconds 1
 
 # Start standalone app
-$apppath = "C:\Users\" + $username + "\Desktop\main.exe"
-Start-Process -FilePath $apppath
+$dirpath = "C:\Users\" + $username + $onedrive + "\Desktop\Tracksense"
+$apppath = "C:\Users\" + $username + $onedrive + "\Desktop\Tracksense\main.exe"
+Start-Process -FilePath $apppath -WorkingDirectory $dirpath
